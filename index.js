acee959085f2318bd72d090f1cec81ceb4407353
200backflips <fredrik.karlsson@appliedtechnology.se>
2020-01-15 15:57:29 +0100

    2nd push

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const server = require("http").createServer();
-const io = require("socket.io")(server);
-const { addUser } = require("./users");
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const { addUser } = require('./users');
 
-io.on("connection", socket => {
-  
-  socket.emit("join", user => {
-    `${user} has joined the chat`;
-  });
+io.on('connection', socket => {
+	socket.emit('join', `u has joined the chat`);
 
-  socket.on("message", message => {
-    console.log(message);
-  });
+	socket.on('message', message => {
+		console.log(message);
+	});
 
-  socket.on("disconnect", user => {
-    console.log(`${user} has left the chat`);
-  });
+	socket.on('disconnect', user => {
+		console.log(user);
+		console.log(`${user} has left the chat`);
+	});
 });
 
 server.listen(8080);
--- users.js
+++ users.js
@@ -1,17 +1,17 @@
 const users = [];
 
 const addUser = name => {
-  name = name.trim().toLowerCase();
+	name = name.trim().toLowerCase();
 
-  const existingUser = users.find(user => {
-    user.name === name
-  })
+	const existingUser = users.find(user => {
+		user.name === name;
+	});
 
-  if (existingUser) {
-    return { error: 'username is already taken'}
-  }
-  const user = name;
-  users.push(user);
-}
+	if (existingUser) {
+		return { error: 'username is already taken' };
+	}
+	const user = name;
+	users.push(user);
+};
 
 module.exports = addUser;
